0b3bb152dbcd892853716571f4b4b0073e241b80
simonliu007 <simon.liu@intelligentgroup.cn>
2018-02-28 16:35:25 +0800

    add test.

---
+++ common/src/main/java/com/liu/CatchYou/common/test.java
@@ -0,0 +1,20 @@
+package com.liu.CatchYou.common;
+
+
+/**
+ * Created by simon.liu on 2017/9/11.
+ */
+public class test {
+    public void testOne() {
+        System.out.println("hello kitty.");
+    }
+    public static void main(String[] args) {
+        long d = 120L;
+        System.out.println("lx");
+        new test().testOne();
+    }
+
+    public Double res(){
+        return null;
+    }
+}
